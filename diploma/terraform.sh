88510a7a281632f77e3933e02a05ecb4c74361cc
Aleksandr <panarin0290@gmail.com>
2022-07-30 16:30:46 +0300

    Update terraform.sh
    edit script

--- diploma/terraform.sh
+++ diploma/terraform.sh
@@ -1,10 +1,12 @@
 echo "===> RUN TERRAFORM"
 cd terraform
-printf '======> On proxy and press ENTER'
+printf '======> ON PROXY and press ENTER'
 read
 terraform init -backend-config=backend.conf
 terraform workspace new stage
 terraform workspace select stage 
 terraform plan
+printf '======> OFF PROXY and press ENTER'
+read
 terraform apply -auto-approve
 cd ../
